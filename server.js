d799f80396db0ff5c871c9976876158dcbff1797
Manohar <manoharkade70@gmail.com>
2026-04-16 20:46:20 +0530

    fix render binding

--- server.js
+++ server.js
@@ -182,6 +182,6 @@ app.get("/user-history/:mobile", async (req, res) => {
 // ================= SERVER =================
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
+app.listen(PORT, "0.0.0.0", () => {
   console.log("Server running on port", PORT);
 });
